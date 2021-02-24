80e222c176e25773de2ba7d218ffbe3665c04003
郑浩楠 <haonan.zheng@xbongbong.com>
2021-02-24 17:25:11 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  themeConfig: {
+    sidebar: {
+      '/http/': [
+        '',
+        'http-summary'
+      ]
+    },
+    lastUpdated: 'Last Updated',
+    smoothScroll: true
+  }
+}
